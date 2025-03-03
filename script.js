717c011a6a82703bea53481144e63005b59684e2
Sanju Santhosh <68913047+TheDAOS@users.noreply.github.com>
2025-03-03 03:40:44 +0000

    testing

--- script.js
+++ script.js
@@ -2,11 +2,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const button = document.getElementById('Download');
     const success = document.getElementById("S-download");
 
-    button.addEventListener('click', () => {
-        if (success.style.display === 'none' || success.style.display === '') {
-            success.style.display = 'block';
-        } else {
-            success.style.display = 'none';
+    // const folderPath = '/test';
+    const folderPath = 'https://thedaos.github.io/Wallpaper/Wallpaper/'
+    const zip = new JSZip();
+
+    async function zipFile() {
+        try {
+            const response = await fetch(folderPath);
+            const folderContent = await response.json();
+
+            // Assuming folderContent is an array of file names
+            for (const fileName of folderContent) {
+                const fileResponse = await fetch(`${folderPath}/${fileName}`);
+                const fileBlob = await fileResponse.blob();
+                zip.file(fileName, fileBlob);
+            }
+
+            const zipBlob = await zip.generateAsync({ type: 'blob' });
+            saveAs(zipBlob, 'test.zip');
+        } catch (error) {
+            console.error('Error zipping and downloading the folder:', error);
         }
+    }
+
+    button.addEventListener('click', () => {
+        success.style.display = 'block';
+        zipFile();
     });
 });
